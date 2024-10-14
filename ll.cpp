556e694e1f03c15041a7e23a00551b1214586980
Arth Joshi <arthhhh65@gmail.com>
2024-10-15 02:38:23 +0530

    New commit

--- ll.cpp
+++ ll.cpp
@@ -1,70 +1,96 @@
 #include<iostream>
-#include<stdlib.h>
+#include<stdio.h>
 #include<vector>
 using namespace std;
 
 struct Node{
-    int data;
-    Node *next;
-
-    Node(int data1, Node *next1){
+    int data ;
+    Node* next;
+    Node(int data1, Node* next1){
         data = data1;
         next = next1;
     }
-    
-    Node(int data1){
+    Node (int data1){
         data = data1;
         next = nullptr;
     }
 };
 
-Node* convertArr2LL(vector<int>&arr){
-    Node*head= new Node(arr[0]);
+Node* change2LL(vector<int>&arr){
+    Node* head= new Node(arr[0]);
     Node*mover=head;
-    for (int i=1;i<arr.size();i++){
-        Node*temp=new Node(arr[i]);
+    for(int i=1;i<arr.size();i++){
+        Node*temp = new Node(arr[i]);
         mover->next=temp;
-        mover=temp;
+        mover= temp;
     }return head;
 }
 
-int lengthofLL(Node*head){
-    int count =0;
-    Node *temp =head;
-    while(temp){
-        temp = temp->next;
-        count++;
-    }return count;
-}
-
-int printLL(Node*head){
-    Node*temp = head;
-    while(temp){
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }return 1;
-}
-
 int checkIfPresent(Node*head,int val){
-    Node*temp = head;
+    Node* temp = head;
     while(temp){
-        if(temp->data==val)return 1;
-        temp = temp->next;
+        if(temp->data==val){
+            return 1;
+        }
+        temp= temp->next;
     }return 0;
 }
 
-Node* deletehead(Node*head){
-    if (head==NULL)return head;
-    Node*temp = head;
-    head= head->next;
-    delete temp;
-    return head;
+Node* insertNode(Node* head, int val,int k){
+    if (head==NULL){
+        if (k==1)return new Node(val);
+        else return NULL;
+    }
+    if (k==1){
+        return new Node(val,head);
+    }
+    int count=0;
+    Node* temp=head;
+    while(temp !=NULL){
+        count++;
+        if (count ==k-1){
+            Node *x=new Node(val ,temp->next);
+            temp->next=x;
+        }temp = temp->next;
+    }return head;
 }
+
+Node* deleteNode(Node*head,int k){
+    if (head==NULL){
+        return NULL;
+    }
+    if (k==1){
+        Node*temp = head;
+        head = head->next;
+        delete temp;
+    }
+    int count=0;
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp){
+        count++;
+        if (count==k){
+            prev->next=prev->next->next;
+            delete temp;
+            break;
+        }prev=temp;
+        temp=temp->next;
+    }return head;
+}
+
+int printLL(Node*head){
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }return 1;
+}
+
 int main() {
-    vector<int> arr = {2, 45, 6, 7};
-    Node* head = convertArr2LL(arr);
-    
-    head = deletehead(head);
+    vector<int> arr = {2,3,4,5,6,7};
+    Node* head = change2LL(arr);
+    head= deleteNode(head,5);
     printLL(head);
+    
     return 0;
 }
